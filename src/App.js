36e1a1698805d1b7edc01a40ce989ea012bd6b4c
Utkarsh Kumar <8460131930lucky@gmail.com>
2025-03-03 17:06:13 +0530

    Fixed API URL for Render

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   
     try {
       // Send POST request to backend
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://insta-backend-z7gz.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
